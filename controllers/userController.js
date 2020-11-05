b501bfa19e25c765937f95ec4f8418aac780f4b4
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-05 18:27:43 -0300

    fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,16 +5,55 @@ const path = require("path");
 
 const userController = {
   //Static
-  login: (req, res) => {
-    null;
+  store: async (req, res) => {
+    const user = await new db.User({
+      name: req.body.name,
+      lastname: req.body.lastname,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      avatar: "/images/anonimo.png",
+      description: "Suba una nueva descripcion en configuracion",
+    });
+    user.save();
+    res.json(user);
   },
+
+  login: async (req, res) => {
+    const { userEmail, password } = req.body;
+    const user = await db.User.findOne({
+      $or: [
+        {
+          email: userEmail,
+        },
+        {
+          username: userEmail,
+        },
+      ],
+    });
+    if (!user) {
+      return res.status(404).send({
+        auth: false,
+      });
+    }
+    const passwordIsValid = await user.validatePassword(password);
+    if (!passwordIsValid) {
+      return res.status(401).json({
+        auth: false,
+      });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.SECRET, {
+      expiresIn: 60 * 60 * 60,
+    });
+    user.tokens.push(token);
+    user.save();
+    res.json({ auth: true, token });
+  },
+
   show: (req, res) => {
     null;
   },
   delete: (req, res) => {
-    null;
-  },
-  store: (req, res) => {
     null;
   },
 

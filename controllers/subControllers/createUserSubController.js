7f5e2f8db5ddeb40b36c84c05b8535af1c872846
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-11-05 11:06:19 -0300

    Update createUserSubController.js

--- controllers/subControllers/createUserSubController.js
+++ controllers/subControllers/createUserSubController.js
@@ -1,20 +1,4 @@
 const db = require("../../models/mongoose");
 
-const createUserSubController = async (req, res) => {
-  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  const user = await new db.User({
-    name: req.body.name,
-    lastname: req.body.lastname,
-    username: req.body.username,
-    email: req.body.email,
-    password: hashedPassword, //?
-    avatar: "/images/anonimo.png",
-    list_tweets: [],
-    list_users_following: [],
-    list_users_followers: [],
-    description: "Suba una nueva descripcion en configuracion",
-  });
-  user.save();
-  res.json(user);
-};
+const createUserSubController = async (req, res) => {};
 module.exports = createUserSubController;

3f4d6d89804fdcb4f9ed0c9eec6ff207302c29fa
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-11-06 13:40:48 -0300

    createTweet

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,11 +1,34 @@
 const db = require("../models/mongoose");
 
 const tweetController = {
-  store: (req, res) => {
-    null;
+  store: async (req, res) => {
+    const tweet = new db.Tweet({
+      content: req.body.content,
+      author: req.user.id,
+      date_created: Date.now(),
+    });
+    await tweet.save();
+
+    const user = await db.User.findOne({ _id: req.user.id });
+    console.log(tweet);
+    await user.list_tweets.push(await tweet._id);
+
+    await user.save();
+    res.json(tweet);
   },
-  delete: (req, res) => {
-    null;
+
+  delete: async (req, res) => {
+    const tweet = await db.Tweet.findOneAndUpdate(
+      { tweet: req.params.tweetId }
+      /*  {tweet: } */
+    );
+    await tweet.save();
+    /* if (user) {
+        res.status(200).json({ message: "200 OK, token eliminado" });
+      } else {
+        res.status(404).json({ message: "404, datos invalidos" });
+      } */
+    res.json(tweet);
   },
 
   updateLike: (req, res) => {
